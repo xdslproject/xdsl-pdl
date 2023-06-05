304e35e03ea8f32d59af81b0576829494e15739c
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-06-05 11:48:54 +0200

    Add analysis to match fuzzing

--- xdsl_pdl/tools/generate_pdl_matches.py
+++ xdsl_pdl/tools/generate_pdl_matches.py
@@ -5,10 +5,12 @@ from __future__ import annotations
 import subprocess
 import argparse
 
+from dataclasses import dataclass
 from random import randrange
 from io import StringIO
 
 from xdsl.ir import Block, MLContext, Region
+from xdsl.utils.diagnostic import Diagnostic
 from xdsl.xdsl_opt_main import xDSLOptMain
 from xdsl.dialects.builtin import (
     ModuleOp,
@@ -18,6 +20,7 @@ from xdsl.dialects.pdl import (
     PatternOp,
 )
 from xdsl.printer import Printer
+from xdsl_pdl.analysis.pdl_analysis import PDLAnalysisFailed, pdl_analysis_pass
 
 from xdsl_pdl.fuzzing.generate_pdl_matches import (
     create_dag_in_region,
@@ -26,6 +29,12 @@ from xdsl_pdl.fuzzing.generate_pdl_matches import (
     put_operations_in_region,
 )
 from xdsl_pdl.fuzzing.generate_pdl_rewrite import generate_random_pdl_rewrite
+
+
+@dataclass
+class MLIRFailure(Exception):
+    failed_program: str
+    error_msg: str
 
 
 counter = 0
@@ -53,8 +62,6 @@ def run_with_mlir(
     module = ModuleOp([patterns_module, ir_module])
     printer.print_op(module)
 
-    print(mlir_input.getvalue())
-
     res = subprocess.run(
         [
             mlir_executable_path,
@@ -68,8 +75,7 @@ def run_with_mlir(
     )
 
     if res.returncode != 0:
-        print(res.stderr)
-        raise Exception("MLIR failed")
+        raise MLIRFailure(mlir_input.getvalue(), res.stderr)
     print(res.stdout)
     global counter
     print(counter)
@@ -79,12 +85,41 @@ def run_with_mlir(
 def fuzz_pdl_matches(module: ModuleOp, ctx: MLContext, mlir_executable_path: str):
     if not isinstance(module.ops.first, PatternOp):
         raise Exception("Expected a single toplevel pattern op")
+
+    print("Analysis result of the pattern:")
+    # Check if the pattern is valid
+    analysis_correct = True
+    diagnostic = Diagnostic()
+    try:
+        pdl_analysis_pass(ctx, module)
+    except PDLAnalysisFailed as e:
+        diagnostic.add_message(e.op, e.msg)
+        analysis_correct = False
+        print("PDL analysis failed")
+    else:
+        print("PDL analysis succeeded")
+    printer = Printer(diagnostic=diagnostic)
+    printer.print_op(module)
+
     region, ops = pdl_to_operations(module.ops.first, ctx)
     all_dags = generate_all_dags(5)
-    dag = all_dags[randrange(0, len(all_dags))]
-    create_dag_in_region(region, dag, ctx)
-    for populated_region in put_operations_in_region(dag, region, ops):
-        run_with_mlir(populated_region, ctx, mlir_executable_path, module.ops.first)
+    try:
+        for _ in range(0, 10):
+            dag = all_dags[randrange(0, len(all_dags))]
+            create_dag_in_region(region, dag, ctx)
+            for populated_region in put_operations_in_region(dag, region, ops):
+                run_with_mlir(
+                    populated_region, ctx, mlir_executable_path, module.ops.first
+                )
+    except MLIRFailure as e:
+        print("Failed program:")
+        print(e.failed_program)
+        print("Error message:")
+        print(e.error_msg)
+        if analysis_correct:
+            print("Unexpected MLIR failure, analysis did not report it")
+        else:
+            print("Expected MLIR failure, analysis did report it")
 
 
 class PDLMatchFuzzMain(xDSLOptMain):
@@ -102,8 +137,7 @@ class PDLMatchFuzzMain(xDSLOptMain):
             module = self.parse_chunk(chunks[0], extension)
             assert module is not None
 
-        for _ in range(0, 10):
-            fuzz_pdl_matches(module, self.ctx, self.args.mlir_executable)
+        fuzz_pdl_matches(module, self.ctx, self.args.mlir_executable)
 
 
 def main():
