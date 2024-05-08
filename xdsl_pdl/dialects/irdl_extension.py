e4d2bb7faa93aefa56ed06a2d9994bfc24bf1c99
Mathieu Fehr <mathieu.fehr@gmail.com>
2024-05-08 14:29:49 +0100

    Add irdl extension dialect

---
+++ xdsl_pdl/dialects/irdl_extension.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from xdsl.irdl import (
+    AttrSizedOperandSegments,
+    IRDLOperation,
+    irdl_op_definition,
+    var_operand_def,
+)
+from xdsl.ir import Dialect
+
+from xdsl.dialects.irdl import AttributeType
+
+
+@irdl_op_definition
+class CheckSubsetOp(IRDLOperation):
+    name = "irdl_ext.check_subset"
+
+    irdl_options = [AttrSizedOperandSegments()]
+
+    lhs = var_operand_def(AttributeType)
+    rhs = var_operand_def(AttributeType)
+
+
+IRDLExtension = Dialect("irdl_ext", [CheckSubsetOp])
