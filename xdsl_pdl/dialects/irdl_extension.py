462d2263b25839fbe14b60180a594553b56eeca2
Mathieu Fehr <mathieu.fehr@gmail.com>
2024-05-09 13:49:10 +0100

    Improve irdl_extension operation structure

--- xdsl_pdl/dialects/irdl_extension.py
+++ xdsl_pdl/dialects/irdl_extension.py
@@ -4,11 +4,14 @@ from xdsl.irdl import (
     AttrSizedOperandSegments,
     IRDLOperation,
     irdl_op_definition,
+    region_def,
     var_operand_def,
 )
-from xdsl.ir import Dialect
+from xdsl.ir import Dialect, Region
 
 from xdsl.dialects.irdl import AttributeType
+from xdsl.parser import DictionaryAttr, Parser
+from xdsl.printer import Printer
 
 
 @irdl_op_definition
@@ -17,10 +20,39 @@ class CheckSubsetOp(IRDLOperation):
 
     irdl_options = [AttrSizedOperandSegments()]
 
-    lhs = var_operand_def(AttributeType())
-    rhs = var_operand_def(AttributeType())
+    lhs = region_def("single_block")
+    rhs = region_def("single_block")
 
-    assembly_format = "attr-dict `` `(` $lhs `)` `of` `` `(` $rhs `)`"
+    # assembly_format = "attr-dict $lhs `of` $rhs"
+
+    def __init__(
+        self, lhs: Region, rhs: Region, attr_dict: DictionaryAttr | None = None
+    ):
+        super().__init__(
+            regions=[lhs, rhs],
+            attributes=attr_dict.data if attr_dict is not None else None,
+        )
+
+    @classmethod
+    def parse(cls: type[CheckSubsetOp], parser: Parser) -> CheckSubsetOp:
+        attr_dict = parser.parse_optional_attr_dict_with_keyword()
+        lhs = parser.parse_region()
+        parser.parse_keyword("of")
+        rhs = parser.parse_region()
+        return CheckSubsetOp(lhs, rhs, attr_dict)
+
+    def print(self, printer: Printer) -> None:
+        printer.print(self.lhs, " of ", self.rhs)
+        printer.print_attr_dict(self.attributes)
 
 
-IRDLExtension = Dialect("irdl_ext", [CheckSubsetOp])
+@irdl_op_definition
+class YieldOp(IRDLOperation):
+    name = "irdl_ext.yield"
+
+    args = var_operand_def(AttributeType())
+
+    assembly_format = "attr-dict $args"
+
+
+IRDLExtension = Dialect("irdl_ext", [CheckSubsetOp, YieldOp])
