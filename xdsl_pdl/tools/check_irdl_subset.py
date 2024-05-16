0390c9e55f320ad86c5468fca96419f9190ebaff
Mathieu Fehr <mathieu.fehr@gmail.com>
2024-05-16 01:21:06 +0100

    Finish check_irdl_subset

--- xdsl_pdl/tools/check_irdl_subset.py
+++ xdsl_pdl/tools/check_irdl_subset.py
@@ -6,24 +6,33 @@ import argparse
 import sys
 from typing import Any, Callable
 from xdsl.traits import SymbolTable
+from xdsl.utils.hints import isa
 import z3
 
-from xdsl.dialects.builtin import Builtin
-from xdsl.dialects.func import Func, FuncOp, Return
+from xdsl.dialects.builtin import (
+    AnyIntegerAttr,
+    Builtin,
+    IntAttr,
+    IntegerAttr,
+    IntegerType,
+)
+from xdsl.dialects.func import Func
 from xdsl.dialects.irdl import (
     IRDL,
     AnyOfOp,
     AnyOp,
     AttributeOp,
+    BaseOp,
+    IsOp,
     ParametersOp,
     ParametricOp,
     TypeOp,
     DialectOp,
 )
 
-from xdsl.ir import MLContext, Operation, SSAValue
-from xdsl.parser import ModuleOp, Parser
-from xdsl_pdl.dialects.irdl_extension import CheckSubsetOp, IRDLExtension, YieldOp
+from xdsl.ir import Attribute, MLContext, Operation, SSAValue
+from xdsl.parser import IndexType, ModuleOp, Parser
+from xdsl_pdl.dialects.irdl_extension import CheckSubsetOp, EqOp, IRDLExtension, YieldOp
 
 
 def add_attribute_constructors_from_irdl(
@@ -33,18 +42,34 @@ def add_attribute_constructors_from_irdl(
     Add an attribute datatype constructor for each attribute and type definition
     found in the IRDL program.
     """
-    for op in module.walk():
-        if not isinstance(op, ParametersOp):
+    for attr_def in module.walk():
+        if not isinstance(attr_def, TypeOp | AttributeOp):
             continue
-        attr_def = op.parent_op()
+        parameters = attr_def.body.block.last_op
+        num_parameters = len(parameters.operands) if parameters else 0
         assert isinstance(attr_def, AttributeOp | TypeOp)
         dialect_def = attr_def.parent_op()
         assert isinstance(dialect_def, DialectOp)
         name = dialect_def.sym_name.data + "." + attr_def.sym_name.data
         attribute_sort.declare(
             name,
-            *[(f"{name}_arg_{i}", attribute_sort) for i in range(len(op.operands))],
+            *[(f"{name}_arg_{i}", attribute_sort) for i in range(num_parameters)],
         )
+
+
+def convert_attr_to_z3_attr(attr: Attribute, attribute_sort: Any) -> Any:
+    if attr == IndexType():
+        return attribute_sort.__dict__["builtin.index"]
+    if isinstance(attr, IntegerType):
+        bitwidth = convert_attr_to_z3_attr(attr.width, attribute_sort)
+        return attribute_sort.__dict__["builtin.integer_type"](bitwidth)
+    if isa(attr, AnyIntegerAttr):
+        value = convert_attr_to_z3_attr(attr.value, attribute_sort)
+        type = convert_attr_to_z3_attr(attr.type, attribute_sort)
+        return attribute_sort.__dict__["builtin.integer_attr"](value, type)
+    if isinstance(attr, IntAttr):
+        return attribute_sort.__dict__["int"](attr.data)
+    raise Exception(f"Unknown attribute {attr}")
 
 
 def get_constraint_as_z3(
@@ -68,6 +93,26 @@ def get_constraint_as_z3(
             )
         )
         return
+    if isinstance(op, IsOp):
+        values_to_z3[op.output] = convert_attr_to_z3_attr(op.expected, attribute_sort)
+        return
+    if isinstance(op, BaseOp):
+        if op.base_name is not None:
+            attribute_name = op.base_name.data
+        else:
+            assert op.base_ref is not None
+            base_attr_def = SymbolTable.lookup_symbol(op, op.base_ref)
+            assert isinstance(base_attr_def, AttributeOp | TypeOp)
+            dialect_def = base_attr_def.parent_op()
+            assert isinstance(dialect_def, DialectOp)
+            attribute_name = (
+                dialect_def.sym_name.data + "." + base_attr_def.sym_name.data
+            )
+        values_to_z3[op.output] = create_value(op.output)
+        add_constraint(
+            attribute_sort.__dict__["is_" + attribute_name](values_to_z3[op.output])
+        )
+        return
     if isinstance(op, ParametricOp):
         base_attr_def = SymbolTable.lookup_symbol(op, op.base_type)
         assert isinstance(base_attr_def, AttributeOp | TypeOp)
@@ -79,10 +124,14 @@ def get_constraint_as_z3(
 
         values_to_z3[op.output] = attribute_sort.__dict__[attribute_name](*parameters)
         return
+    if isinstance(op, EqOp):
+        val0 = values_to_z3[op.args[0]]
+        for arg in op.args[1:]:
+            add_constraint(val0 == values_to_z3[arg])
+        return
     if isinstance(op, YieldOp):
         return
-    else:
-        assert False, f"Unsupported op {op.name}"
+    assert False, f"Unsupported op {op.name}"
 
 
 def main():
@@ -121,9 +170,12 @@ def main():
     # IRDL program, plus an "Other" attribute that correspond to any other
     # attribute not explicitely defined in the program. Other has a parameter,
     # so two other attributes may be distincts.
+    # int correspond to an integer value. It is used for an integer bitwidth for
+    # instance.
     attribute_sort: Any = z3.Datatype("Attribute")
     add_attribute_constructors_from_irdl(attribute_sort, program)
     attribute_sort.declare("other", ("other_arg_0", z3.IntSort()))
+    attribute_sort.declare("int", ("other_arg_0", z3.IntSort()))
     attribute_sort = attribute_sort.create()
 
     # Mapping from IRDL attribute values to their corresponding z3 value
